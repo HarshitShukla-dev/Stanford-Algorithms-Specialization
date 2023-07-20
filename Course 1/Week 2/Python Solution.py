1ff8172fb9c970a1958e3a7457112029705b6d79
hershit-shukla <hershitshukla35@gmail.com>
2023-07-20 11:31:41 +0000

    Jul 20, 2023, 5:01 PM

--- Course 1/Week 2/Python Solution.py
+++ Course 1/Week 2/Python Solution.py
@@ -1,38 +1,68 @@
 def merge(left, right):
+    """
+    Merges two sorted arrays and returns the merged array and the number of inversions.
+
+    Args:
+        left: The left sorted array.
+        right: The right sorted array.
+
+    Returns:
+        The merged array and the number of inversions.
+    """
+
     inversions = 0
     result = []
-    i, j = 0 , 0
+    i, j = 0, 0
     left_len = len(left)
     right_len = len(right)
-    while i < left_len and j < right_len: # iterate through both arrays and arrange the elements in sorted order
-        if left[i] <= right [j]:
+    while i < left_len and j < right_len:
+        if left[i] <= right[j]:
             result.append(left[i])
-            i+=1
+            i += 1
         else:
-            inversions += (left_len - i)
+            inversions += left_len - i
             result.append(right[j])
-            j+=1
+            j += 1
 
     result += left[i:]
     result += right[j:]
     return result, inversions
 
-#The mergesort method to split the arrays into smaller subarrays
+
 def mergesort(lst):
+    """
+    Sorts the given array using the merge sort algorithm and returns the sorted array and the number of inversions.
+
+    Args:
+        lst: The array to be sorted.
+
+    Returns:
+        The sorted array and the number of inversions.
+    """
+
     if len(lst) <= 1:
         return lst, 0
+
     middle = int(len(lst) / 2)
-    (left,x) = mergesort(lst[:middle])
-    (right,y) = mergesort(lst[middle:])
+    (left, x) = mergesort(lst[:middle])
+    (right, y) = mergesort(lst[middle:])
     (combined, z) = merge(left, right)
-    return combined, x+y+z
+    return combined, x + y + z
 
-f = open("integerArray.txt")
 
-lines = []
+def main():
+    """
+    Reads the input file and prints the number of inversions.
+    """
 
-for line in f:
-    lines.append(int(line))
+    with open("integerArray.txt") as f:
+        lines = []
+        for line in f:
+            lines.append(int(line))
 
-(sorted_lines, inversions) = mergesort(lines)
-print(inversions)
+    (sorted_lines, inversions) = mergesort(lines)
+    print(inversions)
+
+
+if __name__ == "__main__":
+    main()
