06dc0eeac3cabfd8d437b2ccaa96725c44c03c51
hershit-shukla <hershitshukla35@gmail.com>
2023-07-20 08:55:48 +0000

    Jul 20, 2023, 2:25 PM

--- Course 1/Week 1/tempCodeRunnerFile.cpp
+++ Course 1/Week 1/tempCodeRunnerFile.cpp
@@ -1,53 +1,90 @@
 #include <iostream>
-#include <cmath>
 #include <string>
 using namespace std;
 
+// Custom multiplication function for large numbers represented as strings
+string multiply(const string& num1, const string& num2) {
+    int n1 = num1.length();
+    int n2 = num2.length();
+    string result(n1 + n2, '0');
+
+    for (int i = n1 - 1; i >= 0; --i) {
+        int carry = 0;
+        for (int j = n2 - 1; j >= 0; --j) {
+            int sum = (num1[i] - '0') * (num2[j] - '0') + (result[i + j + 1] - '0') + carry;
+            result[i + j + 1] = (sum % 10) + '0';
+            carry = sum / 10;
+        }
+        result[i] += carry;
+    }
+
+    // Remove leading zeros
+    size_t pos = result.find_first_not_of('0');
+    if (pos != string::npos) {
+        return result.substr(pos);
+    }
+
+    return "0";
+}
+
 // Custom integer exponentiation function
-long long int_pow(long long base, int exp) {
-    long long result = 1;
+string int_pow(const string& base, int exp) {
+    string result = "1";
     while (exp > 0) {
         if (exp & 1) {  // If exp is odd
-            result *= base;
+            result = multiply(result, base);
         }
-        base *= base;
         exp >>= 1;
+        base = multiply(base, base);
     }
     return result;
 }
 
-long long karatsubaMultiply(long long num1, long long num2) {
-    // Convert numbers to strings to find their lengths
-    int n1 = to_string(num1).length();
-    int n2 = to_string(num2).length();
+string karatsubaMultiply(const string& num1, const string& num2) {
+    int n1 = num1.length();
+    int n2 = num2.length();
 
     // Base case: Single-digit multiplication
-    if (min(n1, n2) == 1) {
-        return num1 * num2;
+    if (n1 == 1 && n2 == 1) {
+        int product = (num1[0] - '0') * (num2[0] - '0');
+        return to_string(product);
     }
 
+    // Make the length of both numbers equal by padding with leading zeros
+    int max_len = max(n1, n2);
+    string padded_num1 = string(max_len - n1, '0') + num1;
+    string padded_num2 = string(max_len - n2, '0') + num2;
+
+    int half_len = max_len / 2;
+
     // Split the numbers into two halves
-    int half_n1 = n1 / 2;
-    int half_n2 = n2 / 2;
-    long long a = num1 / int_pow(10LL, half_n1);  // Use LL suffix here
-    long long b = num1 % int_pow(10LL, half_n1);  // Use LL suffix here
-    long long c = num2 / int_pow(10LL, half_n2);  // Use LL suffix here
-    long long d = num2 % int_pow(10LL, half_n2);  // Use LL suffix here
+    string a = padded_num1.substr(0, half_len);
+    string b = padded_num1.substr(half_len);
+    string c = padded_num2.substr(0, half_len);
+    string d = padded_num2.substr(half_len);
 
     // Recursive calls for subproblems
-    long long ac = karatsubaMultiply(a, c);
-    long long bd = karatsubaMultiply(b, d);
-    long long ad_bc = karatsubaMultiply(a + b, c + d) - ac - bd;
+    string ac = karatsubaMultiply(a, c);
+    string bd = karatsubaMultiply(b, d);
+    string ad_bc = multiply(karatsubaMultiply(a, d), karatsubaMultiply(b, c));
 
     // Combine the results using the Karatsuba algorithm formula
-    return ac * int_pow(10LL, half_n1 * 2) + ad_bc * int_pow(10LL, half_n1) + bd;  // Use LL suffix here
+    string result = multiply(ac + string(2 * half_len, '0'), "1") +
+                    multiply(ad_bc + string(half_len, '0'), "1") +
+                    bd;
+    
+    // Remove leading zeros
+    size_t pos = result.find_first_not_of('0');
+    if (pos != string::npos) {
+        return result.substr(pos);
+    }
+
+    return "0";
 }
 
 int main() {
-    long long numA = 3141592653589793238LL;  // Use LL suffix here
-    numA = numA * 1000000000000000000LL + 4197169399375105820974944592LL;  // Use LL suffix here
-    long long numB = 2718281828459045235LL;  // Use LL suffix here
-    numB = numB * 1000000000000000000LL + 247093699959574966967627LL;  // Use LL suffix here
+    string numA = "3141592653589793238462643383279502884197169399375105820974944592";
+    string numB = "2718281828459045235360287471352662497757247093699959574966967627";
     cout << karatsubaMultiply(numA, numB) << endl;
     return 0;
 }
