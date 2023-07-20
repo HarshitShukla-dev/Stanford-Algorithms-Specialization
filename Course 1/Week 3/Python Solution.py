701b7044dd761da97cfe3e34a33b183a8811715f
hershit-shukla <hershitshukla35@gmail.com>
2023-07-20 18:29:10 +0000

    Jul 20, 2023, 11:59 PM

--- Course 1/Week 3/Python Solution.py
+++ Course 1/Week 3/Python Solution.py
@@ -1,36 +1,54 @@
-class QuickSorter(object):
+class QuickSorter:
+    """
+    A quicksort implementation.
+
+    Attributes:
+        comparisons (int): The number of comparisons made by the algorithm.
+        array (list): The array to be sorted.
+
+    Methods:
+        read_input(input_file=None): Reads the input from a file or the console.
+        sort(): Sorts the array.
+        partition(start, end): Partitions the array around the pivot element.
+    """
+
     def __init__(self, input_file=None):
-        self._comparisons = 0
-        self._array = []
-        self._inversions = 0
+        self.comparisons = 0
+        self.array = []
         self.read_input(input_file)
 
     @property
     def comparisons(self):
+        """The number of comparisons made by the algorithm."""
         return self._comparisons
 
     @property
     def array(self):
+        """The array to be sorted."""
         return self._array
 
     @array.setter
     def array(self, arr):
+        """Sets the array to be sorted."""
         self._array = arr
 
     def read_input(self, input_file=None):
+        """Reads the input from a file or the console."""
         if input_file is None:
-            self._array = [int(elem) for elem in input().split()]
+            self.array = [int(elem) for elem in input().split()]
             return
         with open(input_file) as numbers:
             for number in numbers:
-                self._array.append(int(number))
+                self.array.append(int(number))
 
     def sort(self):
-        if len(self._array) <= 1:
+        """Sorts the array."""
+        if len(self.array) <= 1:
             return
-        self._qsort(0, len(self._array) - 1)
+        self._qsort(0, len(self.array) - 1)
 
     def _qsort(self, start, end):
+        """Recursively sorts the array."""
         if start >= end:
             return
         pivot = self.partition(start, end)
@@ -38,49 +56,26 @@ class QuickSorter(object):
         self._qsort(pivot + 1, end)
 
     def partition(self, start, end):
-        self._comparisons += end - start
+        """Partitions the array around the pivot element."""
+        self.comparisons += end - start
         pivot = start
         for i in range(start + 1, end + 1):
-            if self._array[i] < self._array[start]:
+            if self.array[i] < self.array[start]:
                 pivot += 1
-                self._array[i], self._array[pivot] = self._array[pivot], self._array[i]
-        self._array[start], self._array[pivot] = self._array[pivot], self._array[start]
+                self.array[i], self.array[pivot] = self.array[pivot], self.array[i]
+        self.array[start], self.array[pivot] = self.array[pivot], self.array[start]
         return pivot
 
 
-class QuickSorterFirstElementPivot(QuickSorter):
-    def partition(self, start, end):
-        return super(QuickSorterFirstElementPivot, self).partition(start, end)
-
-
-class QuickSorterLastElementPivot(QuickSorter):
-    def partition(self, start, end):
-        self._array[start], self._array[end] = self._array[end], self._array[start]
-        return super(QuickSorterLastElementPivot, self).partition(start, end)
-
-
-class QuickSorterMedianElementPivot(QuickSorter):
-    def partition(self, start, end):
-        self._choose_median_pivot(start, end)
-        return super(QuickSorterMedianElementPivot, self).partition(start, end)
-
-    def _choose_median_pivot(self, start, end):
-        length = end - start + 1
-        median_index = length // 2 - 1 if length % 2 == 0 else length // 2
-        median = start + median_index
-        if self._array[start] <= self._array[median] <= self._array[end] or self._array[end] <= self._array[median] <= \
-                self._array[start]:
-            self._array[start], self._array[median] = self._array[median], self._array[start]
-        elif self._array[median] <= self._array[end] <= self._array[start] or self._array[start] <= self._array[end] <= \
-                self._array[median]:
-            self._array[start], self._array[end] = self._array[end], self._array[start]
-
-if __name__ == '__main__':
-    sorters = (QuickSorterFirstElementPivot('intArray.txt'),
-               QuickSorterLastElementPivot('intArray.txt'),
-               QuickSorterMedianElementPivot('intArray.txt'))
+def main():
+    """Sorts the input array and prints the number of comparisons made."""
+    sorters = (QuickSorter('intArray.txt'),
+               QuickSorter('intArray.txt'),
+               QuickSorter('intArray.txt'))
     for sorter in sorters:
         sorter.sort()
     print(sorters[0].comparisons, sorters[1].comparisons, sorters[2].comparisons)
 
-#Output:162085 164123 138382
+
+if __name__ == '__main__':
+    main()
