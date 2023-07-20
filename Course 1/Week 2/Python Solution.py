f2158810b57f5de7d17e68d8b1aeef0f8577bbac
hershit-shukla <hershitshukla35@gmail.com>
2023-07-20 11:02:32 +0000

    Jul 20, 2023, 4:32 PM

--- Course 1/Week 2/Python Solution.py
+++ Course 1/Week 2/Python Solution.py
@@ -1,32 +1,32 @@
 with open('integerArray.txt') as f:
     a = [int(x) for x in f]
 
-def CountSplitInv(B,C):
+def CountSplitInv(B, C):
     i = 0
     j = 0
     count = 0
     D = []
-    while i<len(B) and j<len(C):
-        D.extend([min(B[i],C[j])])
+    while i < len(B) and j < len(C):
+        D.extend([min(B[i], C[j])])
         if B[i] < C[j]:
             i = i + 1
         else:
-            count +=len(B[i:])
-            j+=1
+            count += len(B[i:])
+            j += 1
     D.extend(B[i:])
     D.extend(C[j:])
     Z = count
-    return D,Z
+    return D, Z
 
 def Sort_Count(A):
     n = len(A)
     if n > 1:
-        splitposition = n / 2
-        B,X = Sort_Count(A[:-splitposition])
-        C,Y = Sort_Count(A[-splitposition:])
-        D,Z = CountSplitInv(B,C)
-        return D,X+Y+Z
+        splitposition = int(n / 2)
+        B, X = Sort_Count(A[:-splitposition])
+        C, Y = Sort_Count(A[-splitposition:])
+        D, Z = CountSplitInv(B, C)
+        return D, X + Y + Z
     else:
-        return A,0
+        return A, 0
 
-Sort_Count(a)
+print(Sort_Count(a))
