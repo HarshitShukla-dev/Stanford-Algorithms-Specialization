028419d9f552328e7eb4688ba1cbe35fa7b2fe51
hershit-shukla <hershitshukla35@gmail.com>
2023-07-20 19:19:47 +0000

    Jul 21, 2023, 12:49 AM

--- Course 1/Week 4/tempCodeRunnerFile.py
+++ Course 1/Week 4/tempCodeRunnerFile.py
@@ -1,19 +1,18 @@
-import random
-import copy
+from random import randint
+
 
 class KargerMinCutter:
-    def __init__(self):
+    def __init__(self, graph_file):
         self._graph = {}
         self._total_edges = 0
-
-    def read_graph(self, graph_file):
         with open(graph_file) as file:
-            for line in file:
+            for index, line in enumerate(file):
                 numbers = [int(number) for number in line.split()]
                 self._graph[numbers[0]] = numbers[1:]
                 self._total_edges += len(numbers[1:])
 
     def find_min_cut(self):
+        min_cut = 0
         while len(self._graph) > 2:
             v1, v2 = self._pick_random_edge()
             self._total_edges -= len(self._graph[v1])
@@ -22,25 +21,30 @@ class KargerMinCutter:
             for vertex in self._graph[v2]:
                 self._graph[vertex].remove(v2)
                 self._graph[vertex].append(v1)
-            self._graph[v1] = [v for v in self._graph[v1] if v != v1]
+            self._graph[v1] = list(filter(lambda v: v != v1, self._graph[v1]))
             self._total_edges += len(self._graph[v1])
             self._graph.pop(v2)
-        return len(list(self._graph.values())[0])
+        for edges in self._graph.values():
+            min_cut = len(edges)
+        return min_cut
 
     def _pick_random_edge(self):
-        v1 = random.choice(list(self._graph.keys()))
-        v2 = random.choice(self._graph[v1])
-        return v1, v2
+        rand_edge = randint(0, self._total_edges - 1)
+        for vertex, vertex_edges in self._graph.items():
+            if len(vertex_edges) <= rand_edge:
+                rand_edge -= len(vertex_edges)
+            else:
+                from_vertex = vertex
+                to_vertex = vertex_edges[rand_edge]
+                return from_vertex, to_vertex
 
 if __name__ == "__main__":
     min_cut = 99999
-    min_cutter = KargerMinCutter()
-    min_cutter.read_graph('intArray.txt')
-    num_iterations = 40000
-    for i in range(num_iterations):
-        graph_copy = copy.deepcopy(min_cutter._graph)
+    for i in range(40000):
+        min_cutter = KargerMinCutter('intArray.txt')
         cut = min_cutter.find_min_cut()
         if cut < min_cut:
             min_cut = cut
-    print(min_cut)
+            print(min_cut)
+
 #Output:17
