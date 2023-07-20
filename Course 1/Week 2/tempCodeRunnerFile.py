eec983bde61a09dbb463da12f64e9a56e7743efe
hershit-shukla <hershitshukla35@gmail.com>
2023-07-20 10:53:11 +0000

    Jul 20, 2023, 4:23 PM

--- Course 1/Week 2/tempCodeRunnerFile.py
+++ Course 1/Week 2/tempCodeRunnerFile.py
@@ -3,21 +3,29 @@ import numpy as np
 def read_integer_array(filename):
   return np.array([int(i) for i in open(filename).readlines()])
 
-def count_inversions(array):
-  """Counts the number of inversions in an array."""
+def count_inversions_merge_sort(array):
+  """Counts the number of inversions in an array using the merge sort algorithm."""
   if len(array) <= 1:
     return 0
   else:
-    left_inversions = count_inversions(array[:len(array) // 2])
-    right_inversions = count_inversions(array[len(array) // 2:])
+    middle = len(array) // 2
+    left_inversions = count_inversions_merge_sort(array[:middle])
+    right_inversions = count_inversions_merge_sort(array[middle:])
     inversions = 0
-    for i in range(len(array) // 2):
-      for j in range(len(array) // 2, len(array)):
-        if array[i] > array[j]:
-          inversions += 1
+    i = 0
+    j = middle
+    while i < middle and j < len(array):
+      if array[i] > array[j]:
+        inversions += middle - i
+      elif array[i] == array[j]:
+        inversions += 0
+      else:
+        inversions += 0
+      i += 1
+      j += 1
     return left_inversions + right_inversions + inversions
 
 
 ia = read_integer_array('integerArray.txt')
-num = count_inversions(ia)
+num = count_inversions_merge_sort(ia)
 print(num)
