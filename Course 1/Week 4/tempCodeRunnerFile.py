d23976af070e6b9b1ac808a080daa4f0d7008021
hershit-shukla <hershitshukla35@gmail.com>
2023-07-20 19:21:17 +0000

    Jul 21, 2023, 12:51 AM

--- Course 1/Week 4/tempCodeRunnerFile.py
+++ Course 1/Week 4/tempCodeRunnerFile.py
@@ -1,5 +1,5 @@
-from random import randint
-
+import random
+import copy
 
 class KargerMinCutter:
     def __init__(self, graph_file):
@@ -9,10 +9,9 @@ class KargerMinCutter:
             for index, line in enumerate(file):
                 numbers = [int(number) for number in line.split()]
                 self._graph[numbers[0]] = numbers[1:]
-                self._total_edges += len(numbers[1:])
+                self._total_edges += len(numbers) - 1
 
     def find_min_cut(self):
-        min_cut = 0
         while len(self._graph) > 2:
             v1, v2 = self._pick_random_edge()
             self._total_edges -= len(self._graph[v1])
@@ -21,15 +20,13 @@ class KargerMinCutter:
             for vertex in self._graph[v2]:
                 self._graph[vertex].remove(v2)
                 self._graph[vertex].append(v1)
-            self._graph[v1] = list(filter(lambda v: v != v1, self._graph[v1]))
+            self._graph[v1] = [v for v in self._graph[v1] if v != v1]
             self._total_edges += len(self._graph[v1])
             self._graph.pop(v2)
-        for edges in self._graph.values():
-            min_cut = len(edges)
-        return min_cut
+        return len(list(self._graph.values())[0])
 
     def _pick_random_edge(self):
-        rand_edge = randint(0, self._total_edges - 1)
+        rand_edge = random.randint(0, self._total_edges - 1)
         for vertex, vertex_edges in self._graph.items():
             if len(vertex_edges) <= rand_edge:
                 rand_edge -= len(vertex_edges)
@@ -40,11 +37,12 @@ class KargerMinCutter:
 
 if __name__ == "__main__":
     min_cut = 99999
-    for i in range(40000):
-        min_cutter = KargerMinCutter('intArray.txt')
+    min_cutter = KargerMinCutter('intArray.txt')
+    num_iterations = 40000
+    for i in range(num_iterations):
+        graph_copy = copy.deepcopy(min_cutter._graph)
+        min_cutter._graph = graph_copy
         cut = min_cutter.find_min_cut()
         if cut < min_cut:
             min_cut = cut
-        print(min_cut)
-
-#Output:17
+    print(min_cut)
