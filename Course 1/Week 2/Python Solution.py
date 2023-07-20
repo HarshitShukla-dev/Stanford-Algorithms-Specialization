c66ca71b5b5da4cb70a1a32600ff17cb939f8f52
hershit-shukla <hershitshukla35@gmail.com>
2023-07-20 10:13:44 +0000

    Jul 20, 2023, 3:43 PM

--- Course 1/Week 2/Python Solution.py
+++ Course 1/Week 2/Python Solution.py
@@ -1,3 +1,12 @@
+"""
+Inversions: num of (i, j) in A where i<j and A[i]>A[j]
+"""
+
+def read_integer_array(filename):
+  with open(filename, 'r') as f:
+    ls = f.readlines()
+  return [int(i) for i in ls]
+
 def count_inversions(array):
   """Counts the number of inversions in an array."""
   if len(array) <= 1:
@@ -6,12 +15,13 @@ def count_inversions(array):
     left_inversions = count_inversions(array[:len(array) // 2])
     right_inversions = count_inversions(array[len(array) // 2:])
     inversions = 0
-    with open('integerArray.txt', 'r') as f:
-      for i in range(len(array) // 2):
-        for j in range(len(array) // 2, len(array)):
-          if int(f.readline()) > int(f.readline()):
-            inversions += 1
+    for i in range(len(array) // 2):
+      for j in range(len(array) // 2, len(array)):
+        if array[i] > array[j]:
+          inversions += 1
     return left_inversions + right_inversions + inversions
 
-if __name__ == '__main__':
-  print(count_inversions([int(line.strip()) for line in open('integerArray.txt', 'r')]))
+
+ia = read_integer_array('integerArray.txt')
+num = count_inversions(ia)
+print(num)
