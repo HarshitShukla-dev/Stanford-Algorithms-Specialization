821d0effa0808b7410d5516d6834c4f2eecf07c3
hershit-shukla <hershitshukla35@gmail.com>
2023-07-20 18:34:17 +0000

    Jul 21, 2023, 12:04 AM

--- Course 1/Week 3/Python Solution.py
+++ Course 1/Week 3/Python Solution.py
@@ -1,82 +1,168 @@
-import random
+class QuickSorter:
+    def __init__(self, input_file=None):
+        """
+        Initialize the QuickSorter object.
 
-comparisons = 0
+        :param input_file: Optional. Name of the input file to read the array from.
+        """
+        self._comparisons = 0
+        self._array = []
+        self.read_input(input_file)
 
-def read_input(input_file=None):
-    if input_file is None:
-        return [int(elem) for elem in input().split()]
-    else:
-        with open(input_file) as numbers:
-            return [int(number) for number in numbers]
+    @property
+    def comparisons(self):
+        """
+        Get the number of comparisons performed during sorting.
 
-def quick_sort(arr):
-    global comparisons
-    if len(arr) <= 1:
-        return arr
+        :return: Number of comparisons.
+        """
+        return self._comparisons
 
-    pivot_index = random.randint(0, len(arr) - 1)
-    pivot = arr[pivot_index]
-    left = [x for i, x in enumerate(arr) if x <= pivot and i != pivot_index]
-    right = [x for i, x in enumerate(arr) if x > pivot and i != pivot_index]
+    @property
+    def array(self):
+        """
+        Get the current array.
 
-    comparisons += len(arr) - 1
-    return quick_sort(left) + [pivot] + quick_sort(right)
+        :return: The array being sorted.
+        """
+        return self._array
 
-def quick_sort_first_pivot(arr):
-    global comparisons
-    if len(arr) <= 1:
-        return arr
+    @array.setter
+    def array(self, arr):
+        """
+        Set the array to be sorted.
 
-    pivot = arr[0]
-    left = [x for x in arr[1:] if x <= pivot]
-    right = [x for x in arr[1:] if x > pivot]
+        :param arr: The array to be sorted.
+        """
+        self._array = arr
 
-    comparisons += len(arr) - 1
-    return quick_sort_first_pivot(left) + [pivot] + quick_sort_first_pivot(right)
+    def read_input(self, input_file=None):
+        """
+        Read the input from a file or stdin and populate the array.
 
-def quick_sort_last_pivot(arr):
-    global comparisons
-    if len(arr) <= 1:
-        return arr
+        :param input_file: Optional. Name of the input file to read the array from.
+        """
+        if input_file is None:
+            self._array = [int(elem) for elem in input().split()]
+        else:
+            with open(input_file) as numbers:
+                for number in numbers:
+                    self._array.append(int(number))
 
-    pivot = arr[-1]
-    left = [x for x in arr[:-1] if x <= pivot]
-    right = [x for x in arr[:-1] if x > pivot]
+    def sort(self):
+        """
+        Perform the quick sort algorithm on the array.
+        """
+        if len(self._array) <= 1:
+            return
+        self._qsort(0, len(self._array) - 1)
 
-    comparisons += len(arr) - 1
-    return quick_sort_last_pivot(left) + [pivot] + quick_sort_last_pivot(right)
+    def _qsort(self, start, end):
+        """
+        Recursive helper function for quick sort.
 
-def quick_sort_median_pivot(arr):
-    global comparisons
-    if len(arr) <= 1:
-        return arr
+        :param start: Start index of the partition.
+        :param end: End index of the partition.
+        """
+        if start >= end:
+            return
+        pivot = self.partition(start, end)
+        self._qsort(start, pivot - 1)
+        self._qsort(pivot + 1, end)
 
-    median_index = len(arr) // 2
-    pivot_candidates = [arr[0], arr[-1], arr[median_index]]
-    pivot_candidates.sort()
-    pivot = pivot_candidates[1]
+    def partition(self, start, end):
+        """
+        Partition the array and return the pivot index.
 
-    left = [x for i, x in enumerate(arr) if x < pivot and i != median_index]
-    right = [x for i, x in enumerate(arr) if x > pivot and i != median_index]
+        :param start: Start index of the partition.
+        :param end: End index of the partition.
+        :return: Pivot index.
+        """
+        self._comparisons += end - start
+        pivot = start
+        for i in range(start + 1, end + 1):
+            if self._array[i] < self._array[start]:
+                pivot += 1
+                self._array[i], self._array[pivot] = self._array[pivot], self._array[i]
+        self._array[start], self._array[pivot] = self._array[pivot], self._array[start]
+        return pivot
 
-    comparisons += len(arr) - 1
-    return quick_sort_median_pivot(left) + [pivot] + quick_sort_median_pivot(right)
+
+class QuickSorterFirstElementPivot(QuickSorter):
+    """
+    QuickSorter using the first element as the pivot.
+    """
+
+    def partition(self, start, end):
+        """
+        Override partition method to use the first element as the pivot.
+
+        :param start: Start index of the partition.
+        :param end: End index of the partition.
+        :return: Pivot index.
+        """
+        return super().partition(start, end)
+
+
+class QuickSorterLastElementPivot(QuickSorter):
+    """
+    QuickSorter using the last element as the pivot.
+    """
+
+    def partition(self, start, end):
+        """
+        Override partition method to use the last element as the pivot.
+
+        :param start: Start index of the partition.
+        :param end: End index of the partition.
+        :return: Pivot index.
+        """
+        self._array[start], self._array[end] = self._array[end], self._array[start]
+        return super().partition(start, end)
+
+
+class QuickSorterMedianElementPivot(QuickSorter):
+    """
+    QuickSorter using the median element as the pivot.
+    """
+
+    def partition(self, start, end):
+        """
+        Override partition method to use the median element as the pivot.
+
+        :param start: Start index of the partition.
+        :param end: End index of the partition.
+        :return: Pivot index.
+        """
+        self._choose_median_pivot(start, end)
+        return super().partition(start, end)
+
+    def _choose_median_pivot(self, start, end):
+        """
+        Choose the median element as the pivot and place it at the start of the array.
+
+        :param start: Start index of the partition.
+        :param end: End index of the partition.
+        """
+        length = end - start + 1
+        median_index = length // 2 - 1 if length % 2 == 0 else length // 2
+        median = start + median_index
+        if self._array[start] <= self._array[median] <= self._array[end] or self._array[end] <= self._array[median] <= self._array[start]:
+            self._array[start], self._array[median] = self._array[median], self._array[start]
+        elif self._array[median] <= self._array[end] <= self._array[start] or self._array[start] <= self._array[end] <= self._array[median]:
+            self._array[start], self._array[end] = self._array[end], self._array[start]
+
 
 if __name__ == '__main__':
-    input_array = read_input('intArray.txt')
-    comparisons = 0
+    # Create instances of each QuickSorter subclass and perform sorting
+    sorters = (QuickSorterFirstElementPivot('intArray.txt'),
+               QuickSorterLastElementPivot('intArray.txt'),
+               QuickSorterMedianElementPivot('intArray.txt'))
 
-    sorted_array = quick_sort(input_array.copy())
-    print(f"Random Pivot Comparisons: {comparisons}")
+    for sorter in sorters:
+        sorter.sort()
 
-    comparisons = 0
-    sorted_array_first_pivot = quick_sort_first_pivot(input_array.copy())
-    print(f"First Element Pivot Comparisons: {comparisons}")
-
-    comparisons = 0
-    sorted_array_last_pivot = quick_sort_last_pivot(input_array.copy())
-    print(f"Last Element Pivot Comparisons: {comparisons}")
-
-    comparisons = 0
-    sorted_array_median_pivot = quick_sort_median_pivot(input_array.copy())
-    print(f"Median Element Pivot Comparisons: {comparisons}")
+    # Print the number of comparisons for each sorting method
+    print(f"First Element Pivot Comparisons: {sorters[0].comparisons}")
+    print(f"Last Element Pivot Comparisons: {sorters[1].comparisons}")
+    print(f"Median Element Pivot Comparisons: {sorters[2].comparisons}")
