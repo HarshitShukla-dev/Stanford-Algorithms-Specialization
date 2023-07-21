31ecaede3e9647048fd12c92516feca37354f256
hershit-shukla <hershitshukla35@gmail.com>
2023-07-21 06:23:24 +0000

    Jul 21, 2023, 11:53 AM

--- Course 2/Week 1/Python Solution.py
+++ Course 2/Week 1/Python Solution.py
@@ -1,36 +1,24 @@
-from collections import defaultdict
-
-class SccFinder:
+class SccFinder(object):
     def __init__(self, input_file):
-        """
-        Initializes the Strongly Connected Components (SCC) Finder.
-
-        Args:
-            input_file (str): Path to the input file containing directed edges.
-        """
         self.scc_list = []
         with open(input_file) as file:
             self.finish_order = []
-            self._graph = defaultdict(list)
+            self._graph = {}
             for line in file:
-                from_v, to_v = tuple(map(int, line.split()))
-                self._add_edge_to_graph(from_v, to_v)
+                (from_v, to_v) = tuple(number for number in line.split())
+                self._add_edge_to_graph(int(from_v), int(to_v))
 
     def _add_edge_to_graph(self, from_v, to_v):
-        """
-        Adds a directed edge to the graph.
-
-        Args:
-            from_v (int): Source vertex of the edge.
-            to_v (int): Target vertex of the edge.
-        """
-        self._graph[from_v].append(to_v)
-        self._graph[to_v].append(-from_v)
+        if from_v in self._graph:
+            self._graph[from_v].append(to_v)
+        else:
+            self._graph[from_v] = [to_v]
+        if to_v in self._graph:
+            self._graph[to_v].append(-from_v)
+        else:
+            self._graph[to_v] = [-from_v]
 
     def compute_finish_times(self):
-        """
-        Computes the finish times of the vertices using depth-first search (DFS).
-        """
         visited_nodes, finished_nodes = set(), set()
         for vertex in self._graph.keys():
             if vertex in visited_nodes:
@@ -51,9 +39,6 @@ class SccFinder:
                         finished_nodes.add(node)
 
     def compute_sccs(self):
-        """
-        Computes the sizes of the Strongly Connected Components (SCCs) using finish times.
-        """
         visited_nodes = set()
         assert (len(self.finish_order) == len(self._graph))
         for i in reversed(self.finish_order):
@@ -76,8 +61,10 @@ class SccFinder:
         print(self.scc_list[:5])
 
 if __name__ == "__main__":
-    scc_finder = SccFinder("intArray.txt")
+    scc_finder = SccFinder("assignment_4.txt")
     scc_finder.compute_finish_times()
     scc_finder.compute_sccs()
     expected_sccs = [434821, 968, 459, 313, 211]
     print(scc_finder.scc_list[:5])
+
+#Output: [434821, 968, 459, 313, 211]
